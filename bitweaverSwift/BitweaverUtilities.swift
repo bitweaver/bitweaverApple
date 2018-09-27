17a68ec336c86e4d0878a508153d4293a1e6b066
Christian Fowler <christian@prestophoto.com>
2018-09-27 15:51:06 -0400

    creation of generic BitweaverUtilities

---
+++ bitweaverSwift/BitweaverUtilities.swift
@@ -0,0 +1,13 @@
+//
+//  BitweaverUtilities.swift
+//  PrestoPhoto
+//
+//  Created by Christian Fowler on 9/27/18.
+//  Copyright © 2018 PrestoPhoto. All rights reserved.
+//
+
+import Cocoa
+
+class BitweaverUtilities: NSObject {
+
+}
