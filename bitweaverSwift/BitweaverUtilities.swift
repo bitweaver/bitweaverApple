dec5b5f03492928a4974a4955af820f19066e6cb
spiderr <spiderr@bitweaver.org>
2018-10-04 00:02:50 -0400

    add Date.toStringISO8601 function

--- bitweaverSwift/BitweaverUtilities.swift
+++ bitweaverSwift/BitweaverUtilities.swift
@@ -30,6 +30,16 @@ extension String {
     }
 }
 
+extension Date {
+    func toStringISO8601() -> String? {
+        let RFC3339DateFormatter = DateFormatter()
+        RFC3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        RFC3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        RFC3339DateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return RFC3339DateFormatter.string(from: self)
+    }
+}
+
 
 extension BWViewController {
     func clearChildren() {
