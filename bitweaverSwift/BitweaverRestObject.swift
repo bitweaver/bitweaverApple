097b194da92e1310fda7b3195602fa76d9ed2360
spiderr <spiderr@bitweaver.org>
2019-02-06 23:29:02 -0500

    fix force unwrapped displayUri! in BitweaverRestObject

--- bitweaverSwift/BitweaverRestObject.swift
+++ bitweaverSwift/BitweaverRestObject.swift
@@ -19,7 +19,7 @@ class BitweaverRestObject: NSObject {
     @objc dynamic var userId: NSNumber?             /* User ID on the remote system that created the content */
     @objc dynamic var contentTypeGuid: String = ""   /* Title of the content */
     @objc dynamic var title: String?             /* Title of the content */
-    @objc dynamic var displayUri: URL!               /* URL of the */
+    @objc dynamic var displayUri: URL?               /* URL of the */
     @objc dynamic var createdDate: Date?
     @objc dynamic var lastModifiedDate: Date?
 
