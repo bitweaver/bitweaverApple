aec18e517e1bac3e3d1303c31401398777c47b70
spiderr <spiderr@bitweaver.org>
2019-02-19 17:10:36 -0500

    add Double.rounded(toPlaces) extension

--- bitweaverSwift/BitweaverUtilities.swift
+++ bitweaverSwift/BitweaverUtilities.swift
@@ -104,7 +104,12 @@ extension Double {
     public static func random(min: Double, max: Double) -> Double {
         return Double.random * (max - min) + min
     }
-}
+    
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }}
 
 extension Float {
 
