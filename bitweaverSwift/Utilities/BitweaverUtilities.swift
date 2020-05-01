1b2d9e32dee936d1d989f7d82a7583a395231911
spiderr <spiderr@bitewaver.org>
2020-05-01 00:45:57 -0400

    condense BWColor extensions to a single block

--- bitweaverSwift/Utilities/BitweaverUtilities.swift
+++ bitweaverSwift/Utilities/BitweaverUtilities.swift
@@ -439,6 +439,19 @@ extension BWColor {
 
         self.init(red: red, green: green, blue: blue, alpha: 1)
     }
+    
+    func toHexString() -> String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        
+        return String(format: "#%06x", rgb)
+    }
 }
 
 extension NSRegularExpression {
@@ -582,21 +595,6 @@ extension BWFont {
     func toCssString() -> String {
         let ret = "font: "+pointSize.description+"pt "+fontName
         return ret
-    }
-}
-
-extension BWColor {
-    func toHexString() -> String {
-        var r: CGFloat = 0
-        var g: CGFloat = 0
-        var b: CGFloat = 0
-        var a: CGFloat = 0
-        
-        getRed(&r, green: &g, blue: &b, alpha: &a)
-        
-        let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
-        
-        return String(format: "#%06x", rgb)
     }
 }
 
