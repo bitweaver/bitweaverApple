aecb89dffac9358f37925de18167018dd08cb7e6
spiderr <spiderr@bitewaver.org>
2020-10-05 10:51:20 -0400

    String::toHexString convert to RGB colorspace before getRed call

--- bitweaverSwift/Utilities/BitweaverUtilities.swift
+++ bitweaverSwift/Utilities/BitweaverUtilities.swift
@@ -426,7 +426,9 @@ extension BWColor {
         var b: CGFloat = 0
         var a: CGFloat = 0
         
-        getRed(&r, green: &g, blue: &b, alpha: &a)
+        if let rgbColor = self.usingColorSpaceName(.calibratedRGB) {
+            rgbColor.getRed(&r, green: &g, blue: &b, alpha: &a)
+        }
         
         let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
         
